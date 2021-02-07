4a8de18a4cad74983519d312e7904e052906e413
Ruman-Islam <rumanislam48@gmail.com>
2021-02-07 23:24:52 +0600

    final update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,24 @@
-// * search button
+//* Search Button Event Handler
 const searchButton = document.getElementById('search-btn');
-searchButton.addEventListener('click', function () {
-    const foodInput = document.getElementById('food-input').value;
-    loadData(foodInput);
+searchButton.addEventListener('click', () => {
+    const foodName = document.getElementById('food-input').value;
+    if (foodName === '') {
+        document.getElementById('error').style.display = 'block';
+    }
+    else {
+        loadData(foodName);
+    }
 })
 
 
-// * display food data
+//* Close Error Messege
+function hideError() {
+    const errorBody = document.getElementById("error")
+    errorBody.style.display = "none";
+}
+
+
+//* Display Food Data
 async function loadData(name) {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
     const response = await fetch(url)
@@ -15,14 +27,14 @@ async function loadData(name) {
 }
 
 
-// * display food data
+//* Display Food Data
 const displayData = data => {
     const displayItem = document.getElementById('content');
     const meal = data.meals;
     let empty = '';
     meal.forEach(element => {
         const foodInfo = `
-        <div onclick ="loadMoreData('${element.strMeal}')" class="card food-info">
+        <div onclick ="loadExtraInfo('${element.strMeal}')" class="card food-info">
             <img src="${element.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${element.strMeal}</h5>
@@ -35,8 +47,8 @@ const displayData = data => {
 }
 
 
-// * display more food info
-const loadMoreData = name => {
+//* Display Extra Food Info
+const loadExtraInfo = name => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
     fetch(url)
         .then(response => response.json())
@@ -45,22 +57,28 @@ const loadMoreData = name => {
             const showMealItem = document.getElementById('show-meal-item');
             const foodInfo = `
         <div id="img-info">
-            <img src="${meal.strMealThumb}">
-            <h4>${meal.strMeal}</h4>  
-            <h6>Ingredients</h6>
-            <ul class="list-unstyled">
-                <li>${meal.strMeasure1} ${meal.strIngredient1}</li>
-                <li>${meal.strMeasure2} ${meal.strIngredient2}</li>
-                <li>${meal.strMeasure3} ${meal.strIngredient3}</li>
-                <li>${meal.strMeasure4} ${meal.strIngredient4}</li>
-                <li>${meal.strMeasure5} ${meal.strIngredient5}</li>
-                <li>${meal.strMeasure6} ${meal.strIngredient6}</li>
-                <li>${meal.strMeasure7} ${meal.strIngredient7}</li>
-                <li>${meal.strMeasure8} ${meal.strIngredient8}</li>
-                <li>${meal.strMeasure9} ${meal.strIngredient9}</li>
-            </ul>
+            <div>
+                <img src="${meal.strMealThumb}">
+                <h4>${meal.strMeal}</h4>
+            </div> 
+            <div>
+                <h6>Ingredients</h6>
+                <ul>
+                    <li>${meal.strMeasure1} ${meal.strIngredient1}</li>
+                    <li>${meal.strMeasure2} ${meal.strIngredient2}</li>
+                    <li>${meal.strMeasure3} ${meal.strIngredient3}</li>
+                    <li>${meal.strMeasure4} ${meal.strIngredient4}</li>
+                    <li>${meal.strMeasure5} ${meal.strIngredient5}</li>
+                    <li>${meal.strMeasure6} ${meal.strIngredient6}</li>
+                    <li>${meal.strMeasure7} ${meal.strIngredient7}</li>
+                    <li>${meal.strMeasure8} ${meal.strIngredient8}</li>
+                    <li>${meal.strMeasure9} ${meal.strIngredient9}</li>
+                </ul>
+            </div>
         </div>
         `;
             showMealItem.innerHTML = foodInfo;
         })
 }
+
+//* Thank You
